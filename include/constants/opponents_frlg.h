f1b1eeb4cd3db20cce41ef13702d4c261513e1e9
Webby Developers <dev@webbymarketing.it>
2026-09-11 00:38:24 +0200

    fix: scope custom FRLG trainer constants

--- include/constants/opponents_frlg.h
+++ include/constants/opponents_frlg.h
@@ -627,6 +627,7 @@
 #define TRAINER_CHAMPION_REMATCH_BULBASAUR         621
 #define TRAINER_CHAMPION_REMATCH_CHARMANDER        622
 #define TRAINER_CUE_BALL_PAXTON                    623
+#if defined(FIRERED) || defined(LEAFGREEN)
 #define TRAINER_ALBERA_DARIO                        624
 #define TRAINER_ALBERA_MARA                         625
 #define TRAINER_ALBERA_ELIO                         626
@@ -636,13 +637,12 @@
 #define TRAINER_CISTERNONI_AUREA_RECRUIT             630
 #define TRAINER_VIA_CONSOLARE_LIVIO                  631
 #define TRAINER_VIA_CONSOLARE_ELIO                   632
-#if defined(FIRERED) || defined(LEAFGREEN)
 #define TRAINER_LAGO_WATER_GYM_REMO                 633
 #define TRAINER_LAGO_WATER_GYM_DALIA                634
 #define TRAINER_LAGO_WATER_GYM_NEREO                635
 #define TRAINER_LAGO_WATER_GYM_MARINA               636
-#endif
 #define TRAINER_EMISSARIO_AUREA_RECRUIT              637
+#endif
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is
 //       only space for 25 additional trainers before trainer flag space overflows.
