cf7caa4868db992eb5cfcc00534271e53fc09d0d
Webby Developers <dev@webbymarketing.it>
2026-09-13 12:26:46 +0200

    fix: move Bosco exit one tile south

--- test/validate_bosco_del_romitorio.ps1
+++ test/validate_bosco_del_romitorio.ps1
@@ -13,12 +13,12 @@ function Assert-True([bool]$condition, [string]$message) {
 }
 
 Assert-True ($map.id -eq 'MAP_BOSCO_DEL_ROMITORIO') 'map id'
-Assert-True (@($map.warp_events | Where-Object { $_.x -eq 31 -and $_.y -eq 62 -and $_.dest_map -eq 'MAP_LAGO_DI_ALBERA' -and $_.dest_warp_id -eq 12 }).Count -eq 1) 'west warp'
-Assert-True (@($map.warp_events | Where-Object { $_.x -eq 32 -and $_.y -eq 62 -and $_.dest_map -eq 'MAP_LAGO_DI_ALBERA' -and $_.dest_warp_id -eq 13 }).Count -eq 1) 'east warp'
-Assert-True (@($map.warp_events | Where-Object { $_.x -eq 31 -and $_.y -eq 62 -and $_.elevation -eq 0 }).Count -eq 1) 'west warp elevation'
-Assert-True (@($map.warp_events | Where-Object { $_.x -eq 32 -and $_.y -eq 62 -and $_.elevation -eq 0 }).Count -eq 1) 'east warp elevation'
-Assert-True (@($map.coord_events | Where-Object { $_.x -eq 31 -and $_.y -eq 62 -and $_.script -eq 'BoscoDelRomitorio_EventScript_ExitWest' }).Count -eq 1) 'west exit coord event'
-Assert-True (@($map.coord_events | Where-Object { $_.x -eq 32 -and $_.y -eq 62 -and $_.script -eq 'BoscoDelRomitorio_EventScript_ExitEast' }).Count -eq 1) 'east exit coord event'
+Assert-True (@($map.warp_events | Where-Object { $_.x -eq 31 -and $_.y -eq 63 -and $_.dest_map -eq 'MAP_LAGO_DI_ALBERA' -and $_.dest_warp_id -eq 12 }).Count -eq 1) 'west warp'
+Assert-True (@($map.warp_events | Where-Object { $_.x -eq 32 -and $_.y -eq 63 -and $_.dest_map -eq 'MAP_LAGO_DI_ALBERA' -and $_.dest_warp_id -eq 13 }).Count -eq 1) 'east warp'
+Assert-True (@($map.warp_events | Where-Object { $_.x -eq 31 -and $_.y -eq 63 -and $_.elevation -eq 0 }).Count -eq 1) 'west warp elevation'
+Assert-True (@($map.warp_events | Where-Object { $_.x -eq 32 -and $_.y -eq 63 -and $_.elevation -eq 0 }).Count -eq 1) 'east warp elevation'
+Assert-True (@($map.coord_events | Where-Object { $_.x -eq 31 -and $_.y -eq 63 -and $_.script -eq 'BoscoDelRomitorio_EventScript_ExitWest' }).Count -eq 1) 'west exit coord event'
+Assert-True (@($map.coord_events | Where-Object { $_.x -eq 32 -and $_.y -eq 63 -and $_.script -eq 'BoscoDelRomitorio_EventScript_ExitEast' }).Count -eq 1) 'east exit coord event'
 $scripts = Get-Content -Raw $scriptsPath
 Assert-True ($scripts -match 'BoscoDelRomitorio_EventScript_ExitWest::\r?\n\s*warp MAP_LAGO_DI_ALBERA, 4, 2\r?\n\s*waitstate') 'west exit script'
 Assert-True ($scripts -match 'BoscoDelRomitorio_EventScript_ExitEast::\r?\n\s*warp MAP_LAGO_DI_ALBERA, 5, 2\r?\n\s*waitstate') 'east exit script'
